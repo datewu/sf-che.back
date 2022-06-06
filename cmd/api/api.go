d8b676f331d1dde9a8cb4e7be89f7b7c37a7d979
datewu <hihahajun@gmail.com>
2022-06-06 19:03:09 +0800

    enable cors for sf-che.com

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -9,13 +9,15 @@ import (
 )
 
 func New(app *gtea.App) http.Handler {
-	r := handler.DefaultRouterGroup()
+	conf := handler.DefaultConf()
+	conf.CORS.TrustedOrigins = []string{"sf-che.com", "*.sf-che.com"}
+	r, _ := handler.NewRouterGroup(conf)
 	g := r.Group("")
 	setRoutes(app, g)
 	return g
 }
 
-func setRoutes(app *gtea.App, g *handler.RouterGroup) {
+func setRoutes(_ *gtea.App, g *handler.RouterGroup) {
 	g.Get("/sse_stream", sse.Demo)
 	g.Get("/ping", handler.HealthCheck)
 }
