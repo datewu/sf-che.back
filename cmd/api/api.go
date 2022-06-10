cbe0943c9e34eed838cb9940ff697aad473115a9
datewu <hihahajun@gmail.com>
2022-06-10 16:49:37 +0800

    make update

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,18 +6,19 @@ import (
 	"github.com/datewu/gtea"
 	"github.com/datewu/gtea/handler"
 	"github.com/datewu/gtea/handler/sse"
+	"github.com/datewu/gtea/router"
 )
 
 func New(app *gtea.App) http.Handler {
-	conf := handler.DefaultConf()
+	conf := router.DefaultConf()
 	conf.CORS.TrustedOrigins = []string{"https://sf-che.com", "https://www.sf-che.com"}
-	r, _ := handler.NewRouterGroup(conf)
+	r, _ := router.NewRoutesGroup(conf)
 	g := r.Group("/v1")
 	setRoutes(app, g)
 	return g
 }
 
-func setRoutes(_ *gtea.App, g *handler.RouterGroup) {
+func setRoutes(_ *gtea.App, g *router.RoutesGroup) {
 	g.Get("/sse_stream", sse.Demo)
 	g.Get("/ping", handler.HealthCheck)
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/datewu/sf-che
 
 go 1.18
 
-require github.com/datewu/gtea v0.0.12
+require github.com/datewu/gtea v0.0.15
 
 require (
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
+	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
 )
